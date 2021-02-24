e77522d901ae08a1d3c63d9668b1f35556aef936
tuanstvn1215 <tuanstvn1215@gmail.com>
2021-02-24 22:24:30 +0700

    .

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose')
 const url =
-   'mongodb+srv://tuanstvn1214:tuanstvn1214@cluster0.4hiz7.gcp.mongodb.net/roblox?retryWrites=true&w=majority'
+	'mongodb+srv://tuanstvn1214:sqgE8SWZVTZbqkPf%#^@cluster0.4hiz7.gcp.mongodb.net/roblox?retryWrites=true&w=majority'
 
 mongoose.connect(
-   url, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-   },
-   (err) => {
-      if (err) {
-         console.log(`không thể kết nối Mongodb, lỗi:${err}`)
-      } else {
-         console.log('kết nối Mongodb thành công')
-      }
-   }
+	url,
+	{
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	},
+	(err) => {
+		if (err) {
+			console.log(`không thể kết nối Mongodb, lỗi:${err}`)
+		} else {
+			console.log('kết nối Mongodb thành công')
+		}
+	}
 )
 module.exports = mongoose
